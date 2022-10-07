f65f4ffdb8ae5a6a2eac5c4cda45dbc231981ca9
Toufiq Hasan  Kiron <toufiqhasankiron0@gmail.com>
2022-10-07 10:26:51 +0600

    complete

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src='https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png' className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.jsx</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -14,6 +15,14 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
+        </a>
+        <a
+          className="App-link"
+          href="https://github.com/THK-S-SOFTWARE"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Follow me on GitHub
         </a>
       </header>
     </div>
